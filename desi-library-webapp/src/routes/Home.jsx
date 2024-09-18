b92282ee1f63fa6da6479af0619f9b394aa0f7f1
Robert Yang <priest321@hotmail.com>
2024-09-18 18:58:55 +1000

    Add navigation button for later use

--- desi-library-webapp/src/routes/Home.jsx
+++ desi-library-webapp/src/routes/Home.jsx
@@ -1,9 +1,24 @@
 import Box from "@mui/material/Box"
 import Typography from "@mui/material/Typography"
+import Button from "@mui/material/Button";
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const navigate = useNavigate();
+
+  const handleNavigateToBooks = () => {
+    navigate('/books');
+  };
   return (
     <Box>
+	  <Button 
+        variant="contained" 
+        color="primary" 
+        sx={{ mt: 2 }} 
+        onClick={handleNavigateToBooks}
+      >
+        Go to Books
+      </Button>
       <Typography>Welcome to the DESI Library</Typography>
     </Box>
   );
